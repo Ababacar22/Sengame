3ab4fa6554780fce64dc54739d26cad2e1f6b60a
Ababacar22 <lifaking150@gmail.com>
2025-10-21 17:49:24 +0200

    first commit

---
+++ JeuxSN/questions.js
@@ -0,0 +1,10 @@
+// Structure de base d'une question
+const quizData = [
+    {
+        question: "Qui est surnommé 'Le Roi du Mbalakh' ?",
+        options: ["Ismaël Lô", "Youssou Ndour", "Wally Seck", "Tiken Jah Fakoly"],
+        answer: "Youssou Ndour",
+        category: "Culture & Musique"
+    },
+    // ... Ajoutez au moins 15-20 questions de plus !
+];
